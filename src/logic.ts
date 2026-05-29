339cf08dab26d1d50b76929212eb797ef786f9ab
Sebastian <sebastian@example.com>
2026-05-29 12:00:50 -0300

    wip2

--- src/logic.ts
+++ src/logic.ts
@@ -1,7 +1,40 @@
 import { Task, CpmResult, Node as CpmNode, Arrow } from './types'
+import { getTasksAtNode, expandDependencies } from './utils'
 
-export function calculateCPM(_tasks: Task[]): CpmResult | null {
+export function calculateCPM(tasks: Task[]): CpmResult | null {
     // TODO: implement full CPM algorithm
+
+    let currentID: number = 1
+
+    const originNode: CpmNode = {
+        id: currentID,
+        earliestStartTime: -1,
+        latestStartTime: -1,
+        input: null,
+        output: []
+    }
+
+    const nodes: CpmNode[] = [originNode]
+    const arrows: Arrow[] = []
+
+    nodes.forEach((node) => {
+        const dep = getTasksAtNode(node)
+
+        tasks.forEach((task) => {
+            expandDependencies(task, nodes);
+
+            if (task.dependencies === dep) {
+                let newNode: CpmNode = {
+                    id: currentID++,
+                    earliestStartTime: -1,
+                    latestStartTime: -1,
+                    input: [],
+                    output: []
+                }
+            }
+        })
+    })
+
     return null
 }
 
--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,2 @@
-import { Task, Arrow, Node } from "./types";
-import getTasksAtNode from './getTasksAtNode'
-
 function main(): void {
-    let tasks: Task[] = [
-        {
-            id: "A",
-            duration: 4,
-            dependencies: []
-        },
-        {
-            id: "B",
-            duration: 6,
-            dependencies: ["A"]
-        }
-    ]
-
-    let latestNodeNumber = 1
-
-    // nodo inicial
-    let origin: Node = {
-        id: latestNodeNumber,
-        earliestStartTime: -1,
-        latestStartTime: -1,
-        input: null,
-        output: []
-    }
 }
